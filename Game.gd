c2d560bff528c8e0bf300892250c791b8ca57694
Demian Ruban <rubandemian@gmail.com>
2022-06-01 23:00:25 +0200

    Spark as player

--- Game.gd
+++ Game.gd
@@ -7,7 +7,7 @@ func _ready():
 
 func update_score():
 	score += 1
-	$Score/Label.text = str(score)
+	$Score/Label.text = "Score: " + str(score)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
--- Player.gd
+++ Player.gd
@@ -4,6 +4,14 @@ export (int) var speed = 200
 
 var target
 var velocity = Vector2(1,1)
+onready var _animation_player = $AnimationPlayer
+
+enum State {
+	Moving,
+	Idle
+}
+
+var current_state = State.Idle
 
 func _ready():
 	pass
@@ -14,16 +22,24 @@ func _input(event):
 	# TODO make it more reusable
 	if event.is_action_pressed("click"):
 		target = get_global_mouse_position()
+		current_state = State.Moving
 	elif event is InputEventScreenTouch:
 		target = event.position
+		current_state = State.Moving
 		
 func _process(delta):
-	if target:
-		
-		velocity = position.direction_to(target) * speed
-		
-		if position.distance_to(target) > 5:
-			position += velocity * delta
+	match (current_state):
+		State.Moving:
+			_animation_player.play("Moving")
+			
+			velocity = position.direction_to(target) * speed
+			
+			if position.distance_to(target) > 5:
+				position += velocity * delta
+			else:
+				current_state = State.Idle
+		State.Idle:
+			_animation_player.play("Standing")
 
 func death():
 	hide()
